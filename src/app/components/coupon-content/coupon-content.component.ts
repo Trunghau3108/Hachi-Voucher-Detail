37d79271bb7b5f01a326e30f6d81e3e7f601138d
TrungHau3108 <trunghau3108@gmail.com>
2023-04-04 00:22:14 +0700

    Điều kiện áp dụng - javascript - chức năng click vào checkbox all thì tất cả các checkbox con đều click và ngược lại, radio button xử lí enable và disable

--- src/app/components/coupon-content/coupon-content.component.ts
+++ src/app/components/coupon-content/coupon-content.component.ts
@@ -11,8 +11,15 @@ import * as $ from 'jquery';
   styleUrls: ['./coupon-content.component.scss']
 })
 export class CouponContentComponent implements OnInit {
+  allChecked: boolean = false;
+  isChecked1: boolean = false;
+  isChecked2: boolean = false;
+  isChecked3: boolean = false;
+  isChecked4: boolean = false;
 
- 
+  isPublic = false;
+  isIdentified = false;
+  selectedOption = false;
 
   selectedLang = 'vi'; 
   name = '';
@@ -31,6 +38,13 @@ export class CouponContentComponent implements OnInit {
   descriptionEN = '';
   explanationEN = '';
  
+  public date = new Date()
+  public format: FormatSettings = {
+    displayFormat: "dd/MM/yyyy",
+    inputFormat: "dd/MM/yyyy",
+  };
+
+  // xử lí chuyển đổi form ngôn ngữ
   setCurrentLanguage(lang: string): void {
     this.selectedLang = lang;
   }
@@ -53,15 +67,12 @@ export class CouponContentComponent implements OnInit {
       this.explanation = this.explanationEN;
     }
   }
-  
-  public date = new Date()
-  public format: FormatSettings = {
-    displayFormat: "dd/MM/yyyy",
-    inputFormat: "dd/MM/yyyy",
-  };
+ 
+ 
   
     constructor() {}
     
+    // Xử lí disable date
     disableDates(date: Date) {
       const currentDate = new Date();
       // Lấy ngày, tháng, năm hiện tại
@@ -78,10 +89,39 @@ export class CouponContentComponent implements OnInit {
         || (year === currentYear && month === currentMonth && day < currentDay);
     }
 
+    // xử lí nhập chữ vào input số
     onInputKeypress(event: KeyboardEvent): void {
       const charCode = event.which || event.keyCode;
       if (charCode > 31 && (charCode < 48 || charCode > 57)) {
         event.preventDefault();
+      }
+    }
+
+    // xử lí checkbox 
+    onSubCboxChanged() {
+      if (this.isChecked1 && this.isChecked2 && this.isChecked3 && this.isChecked4) {
+        this.allChecked = true;
+      } else {
+        this.allChecked = false;
+      }
+    }
+    onAllCboxChanged() {
+      if (this.allChecked) {
+        this.isChecked1 = true;
+        this.isChecked2 = true;
+        this.isChecked3 = true;
+        this.isChecked4 = true;
+      } else {
+        this.isChecked1 = false;
+        this.isChecked2 = false;
+        this.isChecked3 = false;
+        this.isChecked4 = false;
+      }
+    }
+    onOptionChange(event: any) {
+      // Bỏ chọn tất cả các radio button khác
+      if (event) {
+        this.selectedOption = event;
       }
     }
   
